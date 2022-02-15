21141b080a68cfb8294fc53e8566438176a3899f
Firebird0981 <larose.christian1@gmail.com>
2022-02-15 09:23:25 -0500

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -27,7 +27,7 @@ const Hero = () => {
               Discover, collect, and sell extraordinary NFTs
             </div>
             <div className={style.description}>
-              OpenSea is the world&apos;s first and largest NFT marketplace
+              Drivyr is the best way to showcase your cars across the blockchain
             </div>
             <div className={style.ctaContainer}>
               <button className={style.accentedButton}>Explore</button>
