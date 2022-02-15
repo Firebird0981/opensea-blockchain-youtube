3c5669525629876b4b1fedbdcd62377736d0e8f6
Firebird0981 <larose.christian1@gmail.com>
2022-02-15 09:12:48 -0500

    Drivyr

--- components/Header.js
+++ components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
       <Link href="/">
         <div className={style.logoContainer}>
           <Image src={openseaLogo} height={40} width={40} />
-          <div className={style.logoText}>Opensea</div>
+          <div className={style.logoText}>Drivyr</div>
         </div>
       </Link>
       <div className={style.searchBar}>
@@ -38,11 +38,11 @@ const Header = () => {
       </div>
       <div className={style.headerItems}>
         <Link href="/collections/0x66a576A977b7Bccf510630E0aA5e450EC11361Fa">
-          <div className={style.headerItem}> Collections </div>
+          <div className={style.headerItem}> Garages </div>
         </Link>
         <div className={style.headerItem}> Stats </div>
         <div className={style.headerItem}> Resources </div>
-        <div className={style.headerItem}> Create </div>
+        <div className={style.headerItem}> Digital Id </div>
         <div className={style.headerIcon}>
           <CgProfile />
         </div>
